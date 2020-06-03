3cfdcdfc9b6337328b4532cd1d8c81bfd2921374
Me <bareust@gmail.com>
2020-06-03 18:58:01 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// // console.log 
+// console.log(leftBorderWidth);
+
+// let arr = [1, 3, 5];
+// console.log (arr [2]);
+
+// alert ("Hello, word!")
+
+// confirm("Устраивает ли вас это?")
+let answer = prompt ("Есть ли вам 18?", "Да");
+console.log(typeof(answer));
